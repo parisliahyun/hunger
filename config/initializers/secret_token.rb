561b6cc55931f25d40072aaa9f5dd04b02713afd
My Name <parisliahyun@gmail.com>
2013-11-25 10:43:52 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hunger::Application.config.secret_key_base = '9039007e80e5ead19c3a865c3e701188dfc06626ebb146b1055d87dc8fcdcc7828e2805b86e2962eaf3aee92e12e442325f093386fd7e709fe69dfcb12ba681c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hunger::Application.config.session_store :cookie_store, key: '_Hunger_session'
