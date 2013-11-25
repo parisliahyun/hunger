7640a864688541fadb96cfb0e7e350de13812ae6
Tom Brennan <tommybrennan@gmail.com.com>
2013-11-25 17:34:49 -0500

    did some upsted to the model added tests for a tribute and a gamemaker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+katniss = Person.create(type: "tribute", name: "katniss everdeen")
---
+++ spec/models/tribute_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+
+describe Person do
+ let(:person) { Person.new(type: "tribute", name: "katnis everdeen", district: 12, gender: "female", is_tribute: true, is_victor: false) }
+end
+
