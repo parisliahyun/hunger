3598f3a787a4fb5f9673693973c3d0b89d4d45e8
Tom Brennan <tommybrennan@gmail.com.com>
2013-11-26 15:17:56 -0500

    working through adding tributes

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,10 @@ ActiveRecord::Schema.define(version: 20131126184429) do
   end
 
   create_table "events", force: true do |t|
+    t.string   "type",       null: false
+    t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "games", force: true do |t|
